e1435d6eb14a458d901fe0e62144774012bc2615
Hasan TEKGÜL <htekgulds@gmail.com>
2024-07-04 20:41:31 +0300

    sample errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import { logger } from './common/logging'
 import { handle } from './common/middlewares/handle'
+import Boom from 'boom'
 const router = Router()
 
 /* GET home page. */
@@ -12,6 +13,18 @@ router.get('/', handle(() => {
   logger.trace('debug')
 
   return 'Hello ExpressJS'
+}))
+
+router.get('/400', handle(() => {
+  return Boom.badRequest('bad request error message')
+}))
+
+router.get('/404', handle(() => {
+  return Boom.notFound('not found error message')
+}))
+
+router.get('/502', handle(() => {
+  return Boom.badGateway('bad gateway error message')
 }))
 
 export default router
